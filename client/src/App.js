fb97aea49bb0cd46934597fb7589ef6ebe597aba
diegoTrueZenit <diego@truezenithcreative.com>
2023-10-11 20:53:19 -0600

    .

--- client/src/App.js
+++ client/src/App.js
@@ -42,6 +42,7 @@ function App() {
               <Route path="/properties" element={<PropertiesPage />} />
             </Route>
             <Route path="/login" element={<Login />} />
+            <Route path="/document" element={<Document />} />
           </Routes>
         </Router>
       
